b6346b8e0ae3c201d9ebff09406a716ed5ccdceb
hymndrazill <srihafiras@yahoo.fr>
2023-02-05 06:07:51 +0100

    adding custom and 3rd party middlewares

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,39 @@
 const express = require("express")
 const app = express()
 const path = require("path")
+const {logger} = require('./middleware/logger')
+const errorHandler = require('./middleware/errorHandler')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
 const PORT = process.env.PORT || 3500;
 
-app.use('/',express.static(path.join(__dirname, '/public')))
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use(cors());
+
+app.use(logger);
+
+// public is a folder, but you can bypass the use of "/" 
+// it won't cause any problems, express will know of it.
+app.use('/',express.static(path.join(__dirname, 'public')))
 
 
 app.use('/', require('./routes/root'))
+ 
+app.all('*',(req,res)=> {
+    res.status(404)
+    if(req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    } else if(req.accepts('json')) {
+        res.json({message : '404 Not Found'})
+    } else {
+        res.type('text').send('404 Not Found')
+    }
+})
+app.use(errorHandler)
 
 app.listen(PORT,()=>{
     console.log(`server is running on ${PORT}`)
