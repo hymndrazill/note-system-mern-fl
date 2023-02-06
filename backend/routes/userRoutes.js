7830b4b72290dda33b55ecbb0b2a6ef94ec70ddb
hymndrazill <srihafiras@yahoo.fr>
2023-02-06 09:53:20 +0100

    setting up the routes for the user

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router()
+const userController = require('../controllers/usersController')
+
+
+router.route('/')
+    .get(userController.getAllUsers)
+    .post(userController.creatNewUser)
+    .patch(userController.updateUser)
+    .delete(userController.deleteUser)
+
+
+    module.exports = router
